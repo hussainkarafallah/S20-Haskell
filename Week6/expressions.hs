adc99bf2766658ab9fe8226334ff391b46f4bc52
Hussain <hussainkarafallah@gmail.com>
2020-02-25 21:12:19 +0100

    hw 6

--- Week6/expressions.hs
+++ Week6/expressions.hs
@@ -1,124 +1,84 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# OPTIONS_GHC -Wall #-}
-{-# LANGUAGE OverloadedStrings #-}
 
-import Data.String
+module Main where
 
--- | A simple expression with variables.
-data Expr a = 
-  Lit Integer                -- ^ Integer literal
-  | Var a                    -- ^ Variable                   
-  | Add (Expr a) (Expr a)    -- ^ Addition. 
-  | Mul (Expr a) (Expr a)    -- ^ Multiplication.
-  deriving (Show, Eq)
-    
-instance Functor Expr  where  
-  fmap _ (Lit one)              = (Lit one)
-  fmap f (Var one)              = Var (f one) 
-  fmap f (Add one two)          = Add (fmap f one) (fmap f two)
-  fmap f (Mul one two)          = Mul (fmap f one) (fmap f two)
+-- expressions extended with 2 operations as requested in the homework
+-- exponentiation and subtraction
+data Expr a
+  = Lit Integer -- ^ Integer literal.
+  | Var a -- ^ Variable
+  | Add (Expr a) (Expr a) -- ^Addition.  
+  | Mul (Expr a) (Expr a) -- ^Multiplication.
+  | Sub (Expr a) (Expr a) -- ^Subtraction
+  | Pow (Expr a) (Expr a) -- ^Exponentiation
+    deriving (Show, Functor)
 
-  
--- | Num instance for nice syntax.
-instance Num (Expr a) 
-  where 
+instance Num (Expr a) where
   e1 + e2 = Add e1 e2
-  e1 * e2 = Mul e1 e2 
-  fromInteger n = Lit n  
+  e1 * e2 = Mul e1 e2
+  e1 - e2 = Sub e1 e2
+  fromInteger n = Lit n
+
+-- a function that finds a pair using the first as key and returns the second
+-- with support of returning a default
+findWithDefault :: Eq a => b -> [(a,b)] -> a -> b
+findWithDefault def list key = case (lookup key list) of
+  Nothing -> def
+  Just val -> val
 
 
-evalwithDraft :: Eq var => Expr var ->[(var, Int)]-> Integer-> Integer 
-evalwithDraft (Lit n) _ _       = n
-evalwithDraft (Var n) x def     
-  = toInteger (snd(findTurple (fromIntegral def) (Var n) x))
-evalwithDraft (Add e1 e2) x def = evalwithDraft e1 x def + evalwithDraft e2 x def
-evalwithDraft (Mul e1 e2) x def = evalwithDraft e1 x def * evalwithDraft e2 x def
-  
--- ==============================================  
--- GIVEN FUNCTIONS
--- ==============================================  
--- | Evaluate an expression with all variables instantiated.  
-eval :: Expr Integer -> Integer 
-eval (Lit n) = n
-eval (Var n) = n
-eval (Add e1 e2) = eval e1 + eval e2  
-eval (Mul e1 e2) = eval e1 * eval e2
+-- the function written in the assignment
+evalWith :: Eq var => Integer -> [(var, Integer)] -> Expr var -> Integer
+evalWith def list (Lit x) = x
+evalWith def list (Var x) = case (lookup x list) of
+  Nothing -> def
+  Just val -> val
+evalWith def list (Add e1 e2) = evalWith def list e1 + evalWith def list e2
+evalWith def list (Sub e1 e2) = evalWith def list e1 - evalWith def list e2
+evalWith def list (Mul e1 e2) = evalWith def list e1 * evalWith def list e2
+evalWith def list (Pow e1 e2) = (evalWith def list e1) ^ (evalWith def list e2)
 
-
--- | Display an expression with variables.
-display :: Expr String -> String
-display (Lit n) = show n
-display (Var s) = s
-display (Add e1 e2) = display e1 ++ " + " ++ display e2
-display (Mul e1 e2) = "(" ++ display e1 ++ ") * (" ++ display e2 ++ ")"
-
--- ==============================================  
--- PART 1
--- ==============================================  
-
--- here we assume that expressions have only var type
--- return is also Var name, Int
-findTurple :: Eq var => a ->Expr var -> [(var, a)] -> (Expr var, a)
-findTurple defaultVal expr [] = (expr, defaultVal)
-findTurple defaultVal expr ((name, intVal):xs)  
-  |Var name == expr    = (expr, intVal)
-  |otherwise = findTurple defaultVal expr xs
-  
-  
-evalWith:: Eq var => Expr var ->[(var, Int)]-> Integer-> Integer 
-evalWith expression maplist def 
-  = eval (fmap (\(expr) ->
-  toInteger (snd(findTurple (fromIntegral def) (Var expr) maplist))) expression)
-
---expToStr:: Expr var -> String
---expToStr expression = (fmap (\(expr) ->
- -- toInteger (snd(findTurple (fromIntegral 0) (Var expr) []))) expression)
-
-
-
+-- the function written in the assignment
 displayWith :: (var -> String) -> Expr var -> String
-displayWith funct expr =  display (fmap (\(expres) -> funct expres) expr )
+displayWith f (Lit x) = show x
+displayWith f (Var x) = f x
+displayWith f (Add e1 e2) = "(" ++ displayWith f e1 ++ "+" ++ displayWith f e2 ++ ")"
+displayWith f (Sub e1 e2) = "(" ++ displayWith f e1 ++ "-" ++ displayWith f e2 ++ ")"
+displayWith f (Mul e1 e2) = "(" ++ displayWith f e1 ++ "*" ++ displayWith f e2 ++ ")"
+displayWith f (Pow e1 e2) = "(" ++ displayWith f e1 ++ "^" ++ displayWith f e2 ++ ")"
+
+-- a general definition of expand vars with use of the functor properties
+-- it acceps a list of (variable , thing to be replaced with) and a default value
+-- and simply converts expressions by substitution from one type to another
+-- so it would be possible to have expression of expressions
+expandVars :: Eq var => a -> [(var , a)] -> Expr var -> Expr a
+expandVars defaultVal list exp = fmap (findWithDefault defaultVal list) exp
 
 
--- check if it really works
-expandVars :: Eq var =>  a ->[(var,  a)]-> Expr(Expr var)->Expr a
-expandVars def maplist expression = 
-  (fmap (\(expr)->(snd(findTurple def expr maplist ))) expression)
+data GExpr f a
+  = GVar a -- variable
+  | GOp (f (GExpr f a)) -- generalised operation/literal
 
--- ==============================================  
--- SAMPLE WORKINGS
--- ==============================================  
+data IExpr expr
+  = ILit Integer
+  | IAdd expr expr
+  | IMul expr expr
+  | ISub expr expr
+  | IPow expr expr
+  deriving (Show, Functor)
+
+-- fromExpr :: Expr a -> GExpr IExpr a
+-- fromExpr (Lit n) = ILit n
+-- fromExpr (Var x) = GVar x
+-- fromExpr (Add e1 e2) = GOp (IAdd e1 e2)
 
 
---working Instance for 1.1
-a = eval (fmap (\(expr) ->toInteger (snd(findTurple (fromIntegral 0) (Var expr) lst))) expres)  
+test1 = evalWith 0 [("x", 2), ("y", 3)] (Add (Var "x") (Var "y"))
+test2 =   evalWith 0 [("x", 2), ("y", 3)] ( Add (Pow ( Add (Var "x") (Var "y") ) (Lit 2) ) (Var "z") )
+test3 = displayWith show (Mul (Var "x") (Add (Lit 2) (Var "y")))
 
-b = evalWith expres lst 0
-
-mew = Lit 4 
-x = Var "x"
-y = Var "y"
-z = Var "z"
-expres = x + 2 * (y + 1)
-tosh = (Mul (Var "x") (Add (Lit 2) (Var "y")))
-lst = [("x", 1 ), ("y", 2 )]
-
-tr = evalWith expres lst 0
-
-yyy= display (Var "x")
-
-lst2 = [("x", y + z), ("y", x + 3)]
-
-unk = Var "unknown"
-
-expres2 = x*y
-mewkmewk = expandVars unk lst2 expres
-
-xy = display tosh 
-mur = displayWith show expres
--- ==============================================  
--- MAIN
--- ==============================================  
 
 main :: IO ()
-main = putStr (show mur) <> putStr "\n" <> putStr (show a)
+main = do
+  putStrLn "hello world"
